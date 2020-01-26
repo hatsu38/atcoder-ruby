0de0115136c90bf536f60dd62a26763001e403c6
hatsu38 <hajiwata0308@gmail.com>
2020-01-26 12:30:15 +0900

    abc107のa~c問題を解いた

---
+++ abc107/a.rb
@@ -0,0 +1,16 @@
+### 例
+# N
+# a_1 b_1
+# ...
+# a_N b_N
+
+# h,w = gets.chomp.split(' ').map(&:to_i)
+# strs = h.times.map{ gets.chomp.split('') }
+
+# input
+# 15 11
+# output
+# 5
+n,i = gets.chomp.split(" ").map(&:to_i)
+
+puts n + 1 - i
---
+++ abc107/c.rb
@@ -0,0 +1,28 @@
+### 例
+# N
+# a_1 b_1
+# ...
+# a_N b_N
+
+# N = gets.to_i
+# strs = N.times.map{ gets.chomp.split('') }
+
+# input
+# 8 5
+# -9 -7 -4 -3 1 2 3 4
+# output
+# 10
+
+
+n,k = gets.chomp.to_s.split(' ').map(&:to_i)
+strs = gets.chomp.to_s.split(' ').map(&:to_i)
+# N本のうち連続するK本のろうそくを選ぶ方法はN-K+1通り
+ans = 10*10
+(n-k+1).times do |i|
+  r = strs[i+k-1]
+  l = strs[i]
+  r_dist = (r-l).abs + r.abs
+  l_dist = (l-r).abs + l.abs
+  ans = [r_dist, l_dist,ans].min
+end
+puts ans
