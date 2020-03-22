b9484db291afe875b1aba0d2ccbb309502ae04c4
hatsu38 <hajiwata0308@gmail.com>
2020-03-23 07:53:29 +0900

    panaonicのa問題だけ解けた

---
+++ panasonic/a.rb
@@ -0,0 +1,21 @@
+### 例
+# N
+# a_1 b_1
+# ...
+# a_N b_N
+
+# h,w = gets.chomp.split(' ').map(&:to_i)
+# strs = h.times.map{ gets.chomp.split('') }
+
+# input
+# 27
+# output
+# 5
+
+
+strs = "1, 1, 1, 2, 1, 2, 1, 5, 2, 2, 1, 5, 1, 2, 1, 14, 1, 5, 1, 5, 2, 2, 1, 15, 2, 2, 5, 4, 1, 4, 1, 51"
+
+k = gets.to_i
+
+ary = strs.split(', ')
+puts ary[k-1]
---
+++ panasonic/b.rb
@@ -0,0 +1,22 @@
+### 例
+# N
+# a_1 b_1
+# ...
+# a_N b_N
+
+# h,w = gets.chomp.split(' ').map(&:to_i)
+# strs = h.times.map{ gets.chomp.split('') }
+
+# input
+# 21
+# output
+# 11
+
+h, w = gets.chomp.split(' ').map(&:to_i)
+
+odd_h_num = h.odd? ? (h+1)/2 : h/2
+odd_w_num = w.odd? ? (w+1)/2 : w/2
+
+pre = (odd_h_num * odd_w_num)
+aft = (h-odd_h_num)*(w-odd_w_num)
+puts pre + aft
