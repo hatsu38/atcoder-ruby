ce84d4a80640d23b2ca56e3488046e6fee3daa73
hatsu38 <hajiwata0308@gmail.com>
2023-06-06 22:18:44 +0900

    feat: 🎸 math_and_algorithm/018, 019を回答

---
+++ math_and_algorithm/018.rb
@@ -0,0 +1,27 @@
+=begin
+コンビニには N 個の品物が売られており、i 番目（1≤i≤N）の商品の値段は Ai円です。 異なる 
+2 つの品物を買う方法のうち、合計金額が 
+500 円となるものは何通りありますか。
+
+2≤N≤200000
+A iは 100,200,300,400 のいずれか
+
+input
+5
+100 300 400 400 200
+output
+3
+
+=end
+
+N = gets.chomp.to_i
+a_ary = gets.chomp.split.map(&:to_i)
+
+hash = Hash.new { |hash, key| hash[key] = [] }
+
+hash[100] = a_ary.count(100)
+hash[200] = a_ary.count(200)
+hash[300] = a_ary.count(300)
+hash[400] = a_ary.count(400)
+
+puts hash[100] * hash[400] + hash[200] * hash[300]
---
+++ math_and_algorithm/019.rb
@@ -0,0 +1,22 @@
+=begin
+N 枚のカードがあり、左から i 番目（1≤i≤N）のカードの色は A iです。
+A i=1 のとき赤色、A i=2 のとき黄色、A i=3 のとき青色です。
+同じ色のカードを 2 枚選ぶ方法は何通りありますか。
+
+input
+6
+1 3 2 1 1 2
+output
+4
+=end
+
+N = gets.chomp.to_i
+a_ary = gets.chomp.split.map(&:to_i).sort
+
+hash = Hash.new { |hash, key| hash[key] = [] }
+
+hash[1] = a_ary.count(1)
+hash[2] = a_ary.count(2)
+hash[3] = a_ary.count(3)
+
+puts hash[1] * (hash[1] - 1) / 2 + hash[2] * (hash[2] - 1) / 2 + hash[3] * (hash[3] - 1) / 2
